cdc5df07818afa23f1baec0c2ee2de25e59ac12b
rdd6584 <rdd573@naver.com>
2022-11-28 06:25:25 +0900

    캐릭터 움직임 기본 구현 (애니메이션 X) - wasd, 점프 등 구현 - 씬 일부 수정 - 인풋 매니저, 씨네마신 패키지 임포트 Signed-off-by: rdd6584 <rdd573@naver.com>

---
+++ cube-game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using CubeGame;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody playerRigidbody;
+
+    [SerializeField]
+    Animator playerAnimator;
+
+    public float rotateSpeed = 180f;
+    public float moveSpeed = 4f;
+    public float jumpForce = 2f;
+
+    Vector2 moveInputValue = Vector2.zero;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Rotate();
+        Move();
+    }
+
+
+    void OnMoveInput(InputValue value)
+    {
+        moveInputValue = value.Get<Vector2>();
+    }
+
+    void OnJumpInput()
+    {
+        if (Utils.IsGrounded(this.transform.position))
+        {
+            Jump();
+        }
+    }
+
+    private void Move()
+    {
+        Vector3 moveDistance =
+            moveInputValue.y * transform.forward * moveSpeed * Time.deltaTime;
+        playerRigidbody.MovePosition(playerRigidbody.position + moveDistance);
+    }
+
+    private void Rotate()
+    {
+        float turn = moveInputValue.x * rotateSpeed * Time.deltaTime;
+
+        playerRigidbody.rotation =
+            playerRigidbody.rotation * Quaternion.Euler(0, turn, 0f);
+    }
+
+    private void Jump()
+    {
+        playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+}
---
+++ cube-game/Assets/Scripts/Utils.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace CubeGame
+{
+    public static class Utils
+    {
+        const int FieldLayer = 7;
+        public static bool IsGrounded(Vector3 position, int layerMask = 1 << FieldLayer)
+        {
+            var ray = new Ray(position + Vector3.up * 0.1f, Vector3.down);
+            const float maxDistance = 0.2f;
+            return Physics.Raycast(ray, maxDistance, layerMask);
+        }
+    }
+}
