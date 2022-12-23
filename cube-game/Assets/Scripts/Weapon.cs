fa2648644537f7c2d56bb2214018af0c1a93a0c6
dongbin1999 <dongbin1999@inu.ac.kr>
2022-12-23 23:36:48 +0900

    camera transform 참조 방식 수정
    +cube 스크립트 불필요한 코드 삭제

--- cube-game/Assets/Scripts/Weapon.cs
+++ cube-game/Assets/Scripts/Weapon.cs
@@ -5,4 +5,8 @@ public abstract class Weapon : Item
 {
     public abstract void Attack(float baseDamage);
     public abstract WeaponType GetWeaponType();
+    public Transform GetFirstViewCameraTransform()
+    {
+        return CameraController.instance.firstViewCamera.transform;
+    }
 }
