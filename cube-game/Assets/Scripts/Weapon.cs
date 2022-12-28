af610331f4bdf729a38063577743c61c7ee5500d
rdd6584 <rdd573@naver.com>
2022-12-28 23:54:11 +0900

    무기에 calculate damage 추가 Signed-off-by: rdd6584 <rdd573@naver.com>

--- cube-game/Assets/Scripts/Weapon.cs
+++ cube-game/Assets/Scripts/Weapon.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 
 public abstract class Weapon : Item
 {
-    public abstract void Attack(float baseDamage);
+    public abstract void Attack(float basePower);
+    public abstract float CalculateDamage(float basePower);
     public abstract WeaponType GetWeaponType();
+
     public Transform GetFirstViewCameraTransform()
     {
         return CameraController.instance.firstViewCamera.transform;
