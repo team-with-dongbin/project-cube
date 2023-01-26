08693c14d5b0a5dad7307d118fdeb79b0d1edd1c
dongbin1999 <dongbin1999@inu.ac.kr>
2023-01-27 01:17:42 +0900

    조합슬롯 코드구조 수정

--- cube-game/Assets/Scripts/Inventory/CombinationResultSlot.cs
+++ cube-game/Assets/Scripts/Inventory/CombinationResultSlot.cs
@@ -16,32 +16,9 @@ public class CombinationResultSlot : Slot
         combinationResultSlot = GetComponents<CombinationResultSlot>()[0];
     }
 
-    public void TryCombination(Dictionary<int, int> ingredients)
+    public void printResult(int itemId, int count)
     {
-        (int, int) result = (-1, -1);
-        //모든 조합식에 대해 체크.
-        foreach ((Dictionary<int, int> combination, (int, int) output) in CombinationDictionary.instance.GetCombinationList())
-        {
-            bool ok = ingredients.Count == combination.Count;
-            //해당 조합식의 모든 아이템을 정해진 개수 이상 가지고 있는 지 체크.
-            if (!ok) continue;
-            foreach ((int item, int need) in combination)
-            {
-                if (!ingredients.ContainsKey(item) || need > ingredients[item])
-                {
-                    ok = false;
-                    break;
-                }
-            }
-            if (ok)
-            {
-                result = output;
-                break;
-            }
-        }
-        if (result.Item1 == -1000) result.Item1 = Random.Range(1001, 1008);
-
-        if (result.Item1 == -1)
+        if (itemId == -1)
         {
             combinationResultSlot.ClearSlot();
             return;
@@ -50,26 +27,18 @@ public class CombinationResultSlot : Slot
         {
             return;
         }
-        else if (result.Item1 >= 1000 && result.Item1 <= 1007)
+        //itemid만 가지고 어느종류의 아이템이든 instantiate하도록 수정해야함.
+        else if (itemId >= 1000 && itemId <= 1007)
         {
-            //id/개수 가지고, 결과물아이템 넣어줘야됨.
-            //initializeSlot() 함수 구현되면 그걸로 바꿔야함.
             combinationResultSlot.ClearSlot();
-            //현재 picture에서 pictureCube 생성하는것 / PlayerController에서 GetItem 호출하는것과 비슷하지만,
-            //미묘하게 상황이 달라서 직접 만들수밖에없었음. 코드 재활용이 잘 안됨.
-            for (int i = 0; i < result.Item2; i++)
+            for (int i = 0; i < count; i++)
             {
-                GameObject cube = ItemDictionary.instance.InstantiateWithData(Cube, ItemDictionary.instance.FindById(result.Item1));
+                GameObject cube = ItemDictionary.instance.InstantiateWithData(Cube, ItemDictionary.instance.FindById(itemId));
                 cube.GetComponent<Cube>().Drop();
                 if (i == 0) combinationResultSlot.NewSlot(cube);
                 else combinationResultSlot.AddCount(cube);
                 cube.SetActive(false);
             }
         }
-        else //큐브 이외의 다른 아이템을 만드는 조합식이 추가될 예정.
-        {
-            return;
-        }
-
     }
 }
