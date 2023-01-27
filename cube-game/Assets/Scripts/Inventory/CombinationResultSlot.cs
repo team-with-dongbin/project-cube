e68dbebc12e0e032e8543acba24ebb402601a4d2
이동빈 <dongbin1999@inu.ac.kr>
2023-01-27 15:59:47 +0900

    InstantiateWithData 함수의 매개변수 단순화
    - 이제 itemData가 아이템 프리팹을 갖고있음
    - InstantiateWithData 할때 itemId만으로 초기값 설정 가능
    - 각각의 ScriptableObject에 prefab 할당

--- cube-game/Assets/Scripts/Inventory/CombinationResultSlot.cs
+++ cube-game/Assets/Scripts/Inventory/CombinationResultSlot.cs
@@ -6,8 +6,6 @@ using System.Linq;
 public class CombinationResultSlot : Slot
 {
     public static CombinationResultSlot instance;
-    [SerializeField]
-    private GameObject Cube;
     CombinationResultSlot combinationResultSlot;
 
     private void Awake()
@@ -27,13 +25,12 @@ public class CombinationResultSlot : Slot
         {
             return;
         }
-        //itemid만 가지고 어느종류의 아이템이든 instantiate하도록 수정해야함.
-        else if (itemId >= 1000 && itemId <= 1007)
+        else
         {
             combinationResultSlot.ClearSlot();
             for (int i = 0; i < count; i++)
             {
-                GameObject cube = ItemDictionary.instance.InstantiateWithData(Cube, ItemDictionary.instance.FindById(itemId));
+                GameObject cube = ItemDictionary.instance.InstantiateWithData(itemId);
                 cube.GetComponent<Cube>().Drop();
                 if (i == 0) combinationResultSlot.NewSlot(cube);
                 else combinationResultSlot.AddCount(cube);
